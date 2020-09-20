ebe2f1484de39df5462f6d10886342673d875185
jason <jhchoi0709@gmail.com>
2020-09-20 02:30:42 -0700

    modify clf-qp.

--- lib/@CtrlAffineSys/ctrlClfQp.m
+++ lib/@CtrlAffineSys/ctrlClfQp.m
@@ -1,22 +1,25 @@
-function [u, slack, V] = ctrlClfQp(obj, s, u_ref, with_slack)
+function [u, slack, V, feas] = ctrlClfQp(obj, s, u_ref, with_slack, s_ref)
     if isempty(obj.clf)
         error('CLF is not defined so ctrlCbfClfQp cannot be used. Create a class function defineClf and set up clf with symbolic expression.');
     end
 
-    if nargin < 3
+    if nargin < 3 || isempty(u_ref)
         u_ref = zeros(obj.udim, 1);
     end
     if nargin < 4
         with_slack = 1;
+    end
+    if nargin < 5 || isempty(s_ref)
+        s_ref = zeros(obj.sdim, 1);
     end
 
     if size(u_ref, 1) ~= obj.udim
         error("Wrong size of u_ref, it should be (udim, 1) array.");
     end
     
-    V = obj.clf(s);
-    LfV = obj.lf_clf(s);
-    LgV = obj.lg_clf(s);
+    V = obj.clf(s-s_ref);
+    LfV = obj.lf_clf(s-s_ref);
+    LgV = obj.lg_clf(s-s_ref);
 
     %% Constraints : A[u; slack] <= b
     %% TODO: generalize the code to higher relative degree.
@@ -83,23 +86,31 @@ function [u, slack, V] = ctrlClfQp(obj, s, u_ref, with_slack)
         weight_input = eye(obj.udim);
     end
     
+    
+    options =  optimset('Display','off');
     if with_slack         
         % cost = 0.5 [u' slack] H [u; slack] + f [u; slack]
         H = [weight_input, zeros(obj.udim, 1);
             zeros(1, obj.udim), obj.params.weight.slack];
         f_ = [-weight_input * u_ref; 0];
-        [u_slack, ~, exitflag, ~] = quadprog(H, f_, A, b);
+        [u_slack, ~, exitflag, ~] = quadprog(H, f_, A, b, [], [], [], [], [], options);
         if exitflag == -2
+            feas = 0;
             disp("Infeasible QP. CBF constraint is conflicting with input constraints.");
+        else
+            feas = 1;
         end
         u = u_slack(1:obj.udim);
         slack = u_slack(end);
     else
         H = weight_input;
         f_ = -weight_input * u_ref;
-        [u, ~, exitflag, ~] = quadprog(H, f_, A, b);
+        [u, ~, exitflag, ~] = quadprog(H, f_, A, b, [], [], [], [], [], options);
         if exitflag == -2
+            feas = 0;
             disp("Infeasible QP. CBF constraint is conflicting with input constraints.");
+        else
+            feas = 1;
         end
         slack = [];
     end
